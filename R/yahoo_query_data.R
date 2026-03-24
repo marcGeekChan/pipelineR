1e83768f5620a68460dc6ce85e1713a7a974e7c2
Marc Olsem <marc.olsem@cgdis.lu>
2026-03-24 10:03:56 +0100

    feat: init yahoo_query_data

---
+++ R/yahoo_query_data.R
@@ -0,0 +1,28 @@
+#' yahoo_query_data
+#'
+#' @param ticker Ticker
+#' @param start_date Start date
+#' @param end_date End date
+#'
+#' @returns data
+#' @export
+#'
+yahoo_query_data <- function(ticker, start_date, end_date) {
+
+  data <- tryCatch(
+    tidyquant::tq_get(
+      x = ticker,
+      from = start_date,
+      to = end_date,
+      get = "stock.prices"
+    ),
+    error = function(e) return(NULL)
+  )
+
+  # Handle NA case explicitly
+  if (is.null(data) || (length(data) == 1 && is.na(data))) {
+    return(NULL)
+  }
+
+  return(data)
+}
