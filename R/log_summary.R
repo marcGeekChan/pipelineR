900d1ab4a4286c85f74d4d1d96f9ce62cd823eef
Marc Olsem <marc.olsem@cgdis.lu>
2026-03-24 15:34:07 +0100

    feat(log_summary): init

---
+++ R/log_summary.R
@@ -0,0 +1,27 @@
+#' log_summary
+#'
+#' @param summary_tbl Summary table
+#' @param n_rows New rows
+#' @param status Status
+#' @param symbols List of symbols
+#'
+#' @returns summary table
+#' @export
+#'
+log_summary <- function(summary_tbl, n_rows, status, symbols ) {
+  # Collapse symbols into > separated string
+  symbol_list <- paste(symbols, collapse = ", ")
+
+  # Build tibble
+  summary_tbl$n_rows <- summary_tbl$n_rows + n_rows
+  summary_tbl$status <- status
+  summary_tbl$symbol <- symbol_list
+
+  summary_tbl$message <- if (summary_tbl$n_rows > 0) {
+      paste(summary_tbl$n_rows, "rows processed for this batch")
+    } else {
+      "No new rows to insert"
+    }
+
+  return(summary_tbl)
+}
